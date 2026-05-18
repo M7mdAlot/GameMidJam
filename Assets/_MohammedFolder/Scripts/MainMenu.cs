d042d77de9a9a8b5fc0771df58e2637ebc9b16db
M7mdAlot <m7mdalot5@gmail.com>
2026-05-18 16:06:54 +0300

    LastChanges

--- Assets/_MohammedFolder/Scripts/MainMenu.cs
+++ Assets/_MohammedFolder/Scripts/MainMenu.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void SceneNext()
     {
         SceneManager.LoadScene(1);
